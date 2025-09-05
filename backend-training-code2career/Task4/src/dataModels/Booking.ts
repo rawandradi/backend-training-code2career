38b8b0f56f6d85ea49bca87f62e66216467e518b
RawandRadi <rawandradi@gmail.com>
2025-09-05 19:39:02 +0300

    Restructure: moved all task folders to root and removed duplicate folder

--- backend-training-code2career/Task4/src/dataModels/Booking.ts
+++
@@ -1,6 +0,0 @@
-export interface Booking {
-  id: number;
-  userId: number;
-  courseId: number;
-  date: string;
-}
--- backend-training-code2career/Task4/src/dataModels/Course.ts
+++
@@ -1,5 +0,0 @@
-export interface Course {
-  id: number;
-  title: string;
-  instructor: string;
-}
